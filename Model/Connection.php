0fb8aa9b5f4f37298a7add8aff106fc5e34e34d0
JosephLindzius <joseph.lindzius@gmail.com>
2019-10-29 14:30:20 +0100

    connection page added

---
+++ Model/Connection.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+session_start();
+
+
+function selectWHere(PDO $pdo, string $query, string $column, string $value): array
+{
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':' . $column, $value);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+}
+
+function whatIsHappening()
+{
+
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+    //echo '<h2>$_SERVER</h2>';
+    //var_dump($_SERVER);
+
+}
+
+function openConnection()
+{
+    // Try to figure out what these should be for you
+    $dbhost = "localhost";
+    $dbuser = "joseph";
+    $dbpass = "adminPass";
+    $db = "crud";
+
+// Try to understand what happens here
+
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db , $dbuser , $dbpass);
+
+}
+
+
+/* Fetch all of the remaining rows in the result set */
+
+$pdo = openConnection();
+var_dump($pdo);
+$sql = "SELECT * FROM crud.student WHERE id = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['id'=> "1"]);
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+var_dump($data);
