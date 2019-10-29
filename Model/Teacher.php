3113a77721a6d75cbe66b06076f8378fd3f1f665
bona-kim <bona.kim@outlook.be>
2019-10-29 17:03:44 +0100

    create Teacher Class' git status

--- Model/Teacher.php
+++ Model/Teacher.php
@@ -3,5 +3,78 @@
 
 class Teacher
 {
+    private $id;
+    private $name;
+    private $email;
+    private $class;
+
+    /**
+     * Teacher constructor.
+     * @param $name
+     * @param $email
+     * @param $class
+     */
+    public function __construct($name, $email, $class)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->class = $class;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * @param int $class
+     */
+    public function setClass($class): void
+    {
+        $this->class = $class;
+    }
 
 }
