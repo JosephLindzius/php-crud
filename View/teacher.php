54c8b5b27836c0ac04fa22df54c2c40c9a376fe6
bona-kim <bona.kim@outlook.be>
2019-10-30 13:47:12 +0100

    make a overview table for teacher db

--- View/teacher.php
+++ View/teacher.php
@@ -1,1 +1,49 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title>Teacher</title>
+</head>
+<body>
+    <?php require 'includes/header.php'?>
+    <section>
+<!--        <h4>Hello --><?php //echo $user->getName()?><!--,</h4>-->
+        <p>You can see all the teachers in this page.</p>
+
+        <button type="button" class="btn btn-secondary mb-2 align-right">Add a New Teacher</button>
+
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Class</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php ?>
+            <tr>
+                <th scope="row"><?php ?></th>
+                <td><?php ?></td>
+                <td><?php ?></td>
+                <td>1</td>
+                <td><button type="button" class="btn btn-outline-dark btn-sm">Edit</button></td>
+                <td><button type="button" class="btn btn-outline-dark btn-sm">Delete</button></td>
+            </tr>
+            <?php ?>
+            </tbody>
+        </table>
+
+    </section>
+    <?php require 'includes/footer.php'?>
+</body>
+</html>
