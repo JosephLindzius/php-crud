1e6c9bdc5de083af37a8f6affcb3da72d68968af
bona-kim <bona.kim@outlook.be>
2019-10-30 14:53:01 +0100

    make an array of objects with the data from db

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -1,8 +1,21 @@
 <?php
-
+//require './Model/Connect.php';
 
 class TeacherController
 {
+    public function render()
+    {
+        $con = new Connect();
+        $pdo = $con->openConnection();
+        $teachers = $con->getFromTable($pdo, 'teacher');
 
+        $allTeachers = [];
+        foreach ($teachers as $teacher) {
+            $person = new Teacher($teacher['id'], $teacher['name'], $teacher['email'], $teacher['class']);
+            array_push($allTeachers,$person);
+        }
+
+        require 'View/teacher.php';
+    }
 
 }
